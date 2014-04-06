1336d9f804ca6d20b380bfffec98a11c3958806f
Nate Ober <nate.ober@schools.utah.gov>
2014-04-06 15:40:48 -0400

    Updates to JS and body themes.

--- js/main.js
+++ js/main.js
@@ -11,26 +11,20 @@
   	
   
   	
-  	var mn=new Array();
-  	mn[0]="January"; mn[1]="February"; mn[2]="March"; mn[3]="April"; 
-  	mn[4]="May"; mn[5]="June"; mn[6]="July"; mn[7]="August"; 
-  	mn[8]="September"; mn[9]="October"; mn[10]="November"; mn[11]="December";
-  	
-  	var dow=new Array();
-  	dow[0]="Sun";dow[1]="Mon";dow[2]="Tues";dow[3]="Wednes";
-  	dow[4]="Thurs";dow[5]="Fri";dow[6]="Satur";
-  	
-  	var calstring = '&lt;&nbsp;<span class="now">' + mn[month] + "</span>&nbsp;&nbsp;1"; 
+  	var mn=new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+   	var dow=new Array("Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur");
+		
+  	var calstring = '&lt;&nbsp;<span class="now">' + mn[month] + '</span>&nbsp;&nbsp;1'; 
   	
   	for(i=2; i<=daysInMonth(month, year); i++){
   		if (i==today.getDate()){
-  			calstring += ' &middot; <span class="now">[ ' + i + " ]</span>";
+  			calstring += ' &middot; <span class="now">[ ' + i + ' ]</span>';
   		}
   		else{
-  			calstring += " &middot; " + i;
+  			calstring += ' &middot; ' + i;
   			}
   	}
-  	calstring += '&nbsp; &nbsp;<span class="now">' + dow[weekday] + "day</span>&nbsp;&gt;";
+  	calstring += '&nbsp; &nbsp;<span class="now">' + dow[weekday] + 'day</span>&nbsp;&gt;';
   	$("#calendar").html(calstring); 
   } 
   
